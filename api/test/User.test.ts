e6bc6db12ccb6782d2d95bd286e34daa0029878b
Selic <selich.work@gmail.com>
2020-12-26 07:39:00 +0100

    test(core) setting up tests

---
+++ api/test/User.test.ts
@@ -0,0 +1,11 @@
+import { User } from '../src/entities/User';
+import * as chai from 'chai'
+import 'mocha'
+
+
+describe('User tests', () => {
+    it('checking default inputs', () => {
+        const options = new User();
+
+    });
+});
